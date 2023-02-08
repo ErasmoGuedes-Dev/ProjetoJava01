e50df600608315350501ecdf123b14154bbf7ea4
ErasmoDeveloper <erasmo.network@gmail.com>
2023-02-08 14:50:29 -0300

    Add

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
-		//Mostrar
+		//Mostrar Mensagem
 		System.out.println("Meu primeiro programa");
 	
 	}
