35a226b18bc4de6b811be0b2c7dc626727d2cf85
ErasmoDeveloper <erasmo.network@gmail.com>
2023-02-10 08:02:40 -0300

    add comentário ao pacote

--- primeiro-projeto-java/src/cursojava/projetogit/ProjetoProGit.java
+++ primeiro-projeto-java/src/cursojava/projetogit/ProjetoProGit.java
@@ -2,7 +2,9 @@ package cursojava.projetogit;
 
 public class ProjetoProGit {
 	public static void main(String[] args) {
-		
+		int idade = 10;
+		//comentário ao git 
+		System.out.println(idade);
 		System.out.println("Olá mundo");
 	}
 }
