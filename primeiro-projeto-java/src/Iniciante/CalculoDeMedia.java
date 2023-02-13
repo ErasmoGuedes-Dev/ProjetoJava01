7ec36b109f39d534b550ef5723b0b541a73e1248
ErasmoDeveloper <erasmo.network@gmail.com>
2023-02-13 16:08:06 -0300

    Atualizando

--- primeiro-projeto-java/src/Iniciante/CalculoDeMedia.java
+++ primeiro-projeto-java/src/Iniciante/CalculoDeMedia.java
@@ -1,7 +1,30 @@
 package Iniciante;
 
+import javax.swing.JOptionPane;
+
 public class CalculoDeMedia {
 	public static void main(String[] args) {
-		
+
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
+
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota3);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+
+		double media = media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+
+		/* A média para aprovação é 70 */
+
+		if (media >= 70) {
+			JOptionPane.showMessageDialog(null, "Aluno está APROVADO com a média de : " + media);
+		} else if (media >= 51) {
+			JOptionPane.showMessageDialog(null, "O aluno está de RECUPERAÇÃO com a média de: " + media);
+		} else {
+			JOptionPane.showMessageDialog(null, "REPROVADO, sua média é " + media);
+		}
 	}
 }
